a7e40647fe36177ca1afe79672afd422c427dbc3
Ravi Dirckze <r_dirckze@yahoo.com>
2019-10-20 10:18:56 -0700

    initial commit

---
+++ TitanicSurvival/TitanicMLApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TitanicMLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
